023c55075f526997e596aee9759f0dd5e061df2f
jaenrig-ifx <enriquezgarcia.external@infineon.com>
2024-12-02 17:29:50 +0100

    tools/mtb-integration.py: WIP add-bsp command.
    Signed-off-by: jaenrig-ifx <enriquezgarcia.external@infineon.com>

--- tools/mtb-integration.py
+++ tools/mtb-integration.py
@@ -28,3 +28,94 @@
 #       - These submodules libs in extras/ will also be released with the core package.
 #    - Copy to variants/$board/mtb-lib-includes.txt
 # 5. Select the required libraries which need to be included in the arduino-core-psoc and track their versions against the mtb-lib.a used versions.
+
+
+import argparse, os
+
+version = "0.1.0"
+
+def add_bsp(board, dest_dir=None, copy=False):
+
+    # Check if board BSP exists
+    # If not, print error message and return
+    if not os.path.exists(os.path.join("bsps", "APP_TARGET_" + board)):
+        print("Error: Board BSP not found")
+        return
+
+    # Check if dest_dir is provided
+    # Otherwise, set the default value    
+    if dest_dir is None:
+        dest_dir = os.path.join("variants", board, "mtb-bsp")
+
+    # Check if the destination directory exists
+    if not os.path.exists(dest_dir):
+        os.makedirs(dest_dir)
+
+    # Get destination directory
+        
+    # Get the a list of the include and exclude files and 
+    # directories to be copied to the destination
+    
+        
+    # Copy the files and directories to the destination directory
+        
+    # Generate report file with traceable info of 
+    # the copied files and directories
+
+def parser():
+
+    def main_parser_func(args):
+        parser.print_help()
+
+    def parser_add_bsp_func(args):
+        add_bsp(args.board, args.dest_dir, args.c)
+
+    # Main parser
+    class ver_action(argparse.Action):
+        def __init__(self, option_strings, dest, **kwargs):
+            return super().__init__(
+                option_strings, dest, nargs=0, default=argparse.SUPPRESS, **kwargs
+            )
+
+        def __call__(self, parser, namespace, values, option_string, **kwargs):
+            print("mtb-integration version: " + version)
+            parser.exit()
+
+    parser = argparse.ArgumentParser(description="ModusToolbox Arduino PSOC Core Integration Utility")
+    parser.add_argument("-v", "--version", action=ver_action, help="mtb-integration version")
+    subparser = parser.add_subparsers()
+    parser.set_defaults(func=main_parser_func)
+
+    # Add bsp parser 
+    parser_add_bsp_desc='''
+    Add the MTB board BSP sources to the variant/<board> core directory.
+
+    Details: 
+
+    The relevant files and folders of
+    "mtb-arduino-core-psoc-integration/bsps/APP_TARGET_board>"  
+    directory will be symlinked to 
+    "variants/<board>/mtb-bsp" directory.
+
+    The directory cannot be entirely copied due to the lack of 
+    sources exclusion mechanism of the Arduino build toolchain.
+    Only some of the directories prefixes with "COMPONENT_" and
+    "TOOLCHAIN_" will are required, thus only those will be linked.
+
+    Additionally, only ModusToolbox relevant metafiles (i.e. 
+    .cyignore, deps/, ...) and docs will also be excluded. 
+
+    '''
+    parser_add_bsp = subparser.add_parser("add-bsp", formatter_class=argparse.RawTextHelpFormatter, description=parser_add_bsp_desc)
+    parser_add_bsp.add_argument("board", type=str, help="Board (BSP) name")
+    parser_add_bsp.add_argument("-d", "--dest-dir", type=str, help="Destination dir for the bsp sources. Default: variants/<board>/mtb-bsp") 
+    parser_add_bsp.add_argument("-c", action="store_true", help="Copy the bsp files instead of creating symbolic links")
+    parser_add_bsp.set_defaults(func=parser_add_bsp_func)
+
+    # Parser call
+    args = parser.parse_args()
+    args.func(args)
+
+
+if __name__ == "__main__":
+    parser()
